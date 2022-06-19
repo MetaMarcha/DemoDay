82d49c19842efedb6de2abb88813cb13fc37a71a
Camila Carla Soares <camilacarlasoaress@gmail.com>
2022-06-19 03:56:36 -0300

    página perfil

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ let acess_nav = document.querySelector('.acess_nav')
 document.querySelector('#button_icon').onclick = () =>{
     acess_nav.classList.toggle('active');
 }
+
+let emojis = document.querySelector('.emojis')
+
+document.querySelector('#icones').onclick = () =>{
+    emojis.classList.toggle('active');
+}
