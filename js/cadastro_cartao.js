6dbcbeabd60b062478afcaa5c5255eb817475694
Michelly Nonato <smartnovo01@gmail.com>
2022-07-06 11:40:49 -0300

    arrumando os slides e conexão com os botões

--- js/cadastro_cartao.js
+++ js/cadastro_cartao.js
@@ -52,14 +52,10 @@ function mascaraData(val) {
   var expr = /[0123456789]/;
 
   for (i = 0; i < pass.length; i++) {
-    // charAt -> retorna o caractere posicionado no índice especificado
     var lchar = val.value.charAt(i);
     var nchar = val.value.charAt(i + 1);
 
     if (i == 0) {
-      // search -> retorna um valor inteiro, indicando a posição do inicio da primeira
-      // ocorrência de expReg dentro de instStr. Se nenhuma ocorrencia for encontrada o método retornara -1
-      // instStr.search(expReg);
       if ((lchar.search(expr) != 0) || (lchar > 3)) {
         val.value = "";
       }
@@ -67,8 +63,6 @@ function mascaraData(val) {
     } else if (i == 1) {
 
       if (lchar.search(expr) != 0) {
-        // substring(indice1,indice2)
-        // indice1, indice2 -> será usado para delimitar a string
         var tst1 = val.value.substring(0, (i));
         val.value = tst1;
         continue;
