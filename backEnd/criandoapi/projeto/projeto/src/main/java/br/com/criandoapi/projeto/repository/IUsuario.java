ab1cd7a70796d276688a40f2a801591e8f75f450
Michelly Nonatto <smartnovo01@gmail.com>
2022-08-21 16:16:26 -0300

    para o github pages

--- backEnd/criandoapi/projeto/projeto/src/main/java/br/com/criandoapi/projeto/repository/IUsuario.java
+++
@@ -1,9 +0,0 @@
-package br.com.criandoapi.projeto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.criandoapi.projeto.model.Usuario;
-
-public interface IUsuario extends JpaRepository<Usuario, Integer>{
-	
-}
